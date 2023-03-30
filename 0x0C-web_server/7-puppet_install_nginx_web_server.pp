0538e3bf1af1694eb8b7dfb09241a261a1ef4397
kuzivashe <kuzibaye@gmail.com>
2023-03-29 21:03:54 -0700

    install nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,6 +5,6 @@ exec {'install':
   provider => shell,
 }
 exec {'default':
-  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/channel/UC-QtMoPv-9g1vD7PoyKQtEw permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart',
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/channel/UC-QtMoPv-9g1vD7PoyKQtEw permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart',
   provider => shell,
 }
