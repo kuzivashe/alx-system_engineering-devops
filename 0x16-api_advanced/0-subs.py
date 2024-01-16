9d75ef21f183328acf3c7eebfa13a8650a052b67
Kuzivashe <57645999+kuzivashe@users.noreply.github.com>
2024-01-16 14:11:38 +0200

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,28 @@
 import sys
 
 def number_of_subscribers(subreddit):
-    if len(sys.argv) == 0:
-        return 0
-    elif len(sys.argv) < 0:
-        return len
+    """this function calls the reddit api to get the number of subscribers
+    in a specific topic"""
+
+    import requests
+
+    # Reddit API:
+    # Modhash API is a token required by reddit API to help prevent CSRF
+    # /api/me.json call or response is where the modhashes is obtained 
+    # for data of listing endpoints
+    API = 'https://www.reddit.com/'
+    req = requests.get('{}/req/{}/about.json'.format(API, subreddit),
+                       headers={'user-agent': 'Custom user'},
+                       allow_redirects=False)
+    
+    # incase there is an invalid subreddit
+    if req.status_code != 200:
+        return(0)
+    
+    # if successful
+    req = req.json()
+    result = req.get('data').get('subscribers')
+    if result:
+        return(result)
+    # incase there are no subscribers
+    return(0)
