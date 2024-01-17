8d6a38b7ee24fe1df09d022adaad03e215a063fa
Kuzivashe <57645999+kuzivashe@users.noreply.github.com>
2024-01-17 14:41:28 +0200

    update to strace file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 #print paths and more info associated with file descriptors
 
-openat(AT_FDCWD, "/dev/null", O_RDONLY) = 3</dev/null<char 1:3>>
-fstat(3</dev/null<char 1:3>>, {st_mode=S_IFCHR|0666, st_rdev=makedev(0x1, 0x3),
-...}) = 0
-fadvise64(3</dev/null<char 1:3>>, 0, 0, POSIX_FADV_SEQUENTIAL) = 0
-read(3</dev/null<char 1:3>>, "", 131072) = 0
+openat(AT_FDCWD, "/etc/ld.so.cache", O_RDONLY|O_CLOEXEC) = 3
+fstat(3, {st_mode=S_IFREG|0644, st_size=145720, ...}) = 0
+mmap(NULL, 145720, PROT_READ, MAP_PRIVATE, 3, 0) = 0x7f0c0b0d8000
+close(3) = 0
