6501208f8c0b0eccf7acf5f4ead83d8cffd7bd0e
Kuzivashe <57645999+kuzivashe@users.noreply.github.com>
2024-01-17 14:42:41 +0200

    update to strace file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #print paths and more info associated with file descriptors
 
-openat(AT_FDCWD, "/etc/ld.so.cache", O_RDONLY|O_CLOEXEC) = 3
+openat(AT_FDCWD, /etc/ld.so.cache, O_RDONLY|O_CLOEXEC) = 3
 fstat(3, {st_mode=S_IFREG|0644, st_size=145720, ...}) = 0
 mmap(NULL, 145720, PROT_READ, MAP_PRIVATE, 3, 0) = 0x7f0c0b0d8000
 close(3) = 0
